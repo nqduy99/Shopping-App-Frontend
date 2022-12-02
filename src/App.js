edec05964a73105c31f2aebb700d4dd50b61a73d
nqduy99 <nqduy98999@gmail.com>
2022-12-02 15:05:05 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    // element: <Layout />,
+    // children: [
+    //   {
+    //     path: "/",
+    //     element: <Home />,
+    //   },
+    //   {
+    //     path: "/products/:id",
+    //     element: <Products />,
+    //   },
+    //   {
+    //     path: "/product/:id",
+    //     element: <Product />,
+    //   },
+    // ],
+  },
+]);
+
 function App() {
   return (
     <div>
-      Hello world!
+      <RouterProvider router={router} />
     </div>
   );
 }
