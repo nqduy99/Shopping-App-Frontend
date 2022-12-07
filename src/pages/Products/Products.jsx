654e261e21912d21a9732fe83d9da4b334636032
nqduy99 <nqduy98999@gmail.com>
2022-12-07 22:19:23 +0700

    product list page part part 1

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,7 +1,48 @@
 import React from 'react';
+import './Products.scss';
 
 const Products = () => {
-    return <div>Products</div>;
+    return (
+        <div className="products">
+            <div className="left">
+                <div className="filterItem">
+                    <h2>Product Categories</h2>
+                    <div className="inputItem">
+                        <input type="checkbox" id="1" value={1} />
+                        <label htmlFor="1">Shoes</label>
+                    </div>
+                    <div className="inputItem">
+                        <input type="checkbox" id="2" value={2} />
+                        <label htmlFor="2">Skirts</label>
+                    </div>
+                    <div className="inputItem">
+                        <input type="checkbox" id="3" value={3} />
+                        <label htmlFor="3">Coats</label>
+                    </div>
+                </div>
+                <div className="filterItem">
+                    <h2>Filter by price</h2>
+                    <div className="inputItem">
+                        <span>0</span>
+                        <input type="range" min={0} max={1000} />
+                        <span>1000</span>
+                    </div>
+                </div>
+                <div className="filterItem">
+                    <h2>Sort by</h2>
+                    <div className="inputItem">
+                        <input type="radio" id='asc' value='asc' name='price' />
+                        <label htmlFor="asc">Price (Lowest first)</label>
+                    </div>    
+                    <div className="inputItem">
+                        <input type="radio" id='desc' value='desc' name='price' />
+                        <label htmlFor="desc">Price (Highest first)</label>
+                    </div>
+                </div>
+            </div>
+            <div className="right"></div>
+        </div>
+    );
 };
 
 export default Products;
