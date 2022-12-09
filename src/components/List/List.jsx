52314dfd8a4bd98bb5a63466dfa9915b7866afe5
nqduy99 <nqduy98999@gmail.com>
2022-12-09 20:27:36 +0700

    finished single product page

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -58,42 +58,6 @@ const List = () => {
             oldPrice: 19,
             price: 12,
         },
-        {
-            id: 7,
-            img: 'https://i.bloganchoi.com/bloganchoi.com/wp-content/uploads/2022/07/lisa-4-blackpink-696x1046.jpg?fit=700%2C20000&quality=95&ssl=1',
-            img2: 'https://vcdn1-giaitri.vnecdn.net/2021/11/24/lisa-2554-1637749826.jpg?w=680&h=0&q=100&dpr=1&fit=crop&s=ybWaQjRUALuxjSGVNvqnVQ',
-            title: 'Lisa',
-            isNew: true,
-            oldPrice: 19,
-            price: 12,
-        },
-        {
-            id: 8,
-            img: 'https://i.bloganchoi.com/bloganchoi.com/wp-content/uploads/2022/07/lisa-4-blackpink-696x1046.jpg?fit=700%2C20000&quality=95&ssl=1',
-            img2: 'https://vcdn1-giaitri.vnecdn.net/2021/11/24/lisa-2554-1637749826.jpg?w=680&h=0&q=100&dpr=1&fit=crop&s=ybWaQjRUALuxjSGVNvqnVQ',
-            title: 'Lisa',
-            isNew: true,
-            oldPrice: 19,
-            price: 12,
-        },
-        {
-            id: 9,
-            img: 'https://i.bloganchoi.com/bloganchoi.com/wp-content/uploads/2022/07/lisa-4-blackpink-696x1046.jpg?fit=700%2C20000&quality=95&ssl=1',
-            img2: 'https://vcdn1-giaitri.vnecdn.net/2021/11/24/lisa-2554-1637749826.jpg?w=680&h=0&q=100&dpr=1&fit=crop&s=ybWaQjRUALuxjSGVNvqnVQ',
-            title: 'Lisa',
-            isNew: true,
-            oldPrice: 19,
-            price: 12,
-        },
-        {
-            id: 10,
-            img: 'https://i.bloganchoi.com/bloganchoi.com/wp-content/uploads/2022/07/lisa-4-blackpink-696x1046.jpg?fit=700%2C20000&quality=95&ssl=1',
-            img2: 'https://vcdn1-giaitri.vnecdn.net/2021/11/24/lisa-2554-1637749826.jpg?w=680&h=0&q=100&dpr=1&fit=crop&s=ybWaQjRUALuxjSGVNvqnVQ',
-            title: 'Lisa',
-            isNew: true,
-            oldPrice: 19,
-            price: 12,
-        },
     ];
 
     return <div className='list'>{
--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,8 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Product.scss';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import BalanceIcon from '@mui/icons-material/Balance';
 
 const Product = () => {
-    return <div>Product</div>;
+    const [selectedImg, setSelectedImg] = useState(0);
+    const [quantity, setQuantity] = useState(1);
+
+    const images = [
+        'https://vtv1.mediacdn.vn/2022/10/21/gettyimages-1656681307-1666320628461839905497.jpg',
+        'https://image.thanhnien.vn/w1024/Uploaded/2022/zxaijr/2021_12_30/moneycualisalapkyluc3-8661.png',
+    ];
+
+    return (
+        <div className="product">
+            <div className="left">
+                <div className="images">
+                    <img src={images[0]} alt="productImg01" onClick={(e) => setSelectedImg(0)} />
+                    <img src={images[1]} alt="productImg02" onClick={(e) => setSelectedImg(1)} />
+                </div>
+                <div className="main">
+                    <div className="mainImg">
+                        <img src={images[selectedImg]} alt="" />
+                    </div>
+                </div>
+            </div>
+            <div className="right">
+                <h1>Title</h1>
+                <span className='price'>$199</span>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas
+                    accumsan lacus vel facilisis labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.
+                    Risus commodo viverra maecenas.
+                </p>
+                <div className="quantity">
+                    <button onClick={() => setQuantity((prev) => (prev === 1 ? 1 : prev - 1))}>-</button>
+                    {quantity}
+                    <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
+                </div>
+                <button className="add">
+                    <AddShoppingCartIcon /> ADD TO CART
+                </button>
+                <div className="links">
+                    <div className="item">
+                        <FavoriteBorderIcon /> ADD TO WISHLIST
+                    </div>
+                    <div className="item">
+                        <BalanceIcon /> ADD TO COMPARE
+                    </div>
+                </div>
+                <div className="info">
+                    <span>Vendor: Polo</span>
+                    <span>Product Type: T-Shirt</span>
+                    <span>Tag: T-Shirt, Women, Top</span>
+                </div>
+                <hr />
+                <div className="info">
+                    <span>DESCRIPTION</span>
+                    <hr />
+                    <span>ADDITIONAL INFORMATION</span>
+                    <hr />
+                    <span>FAQ</span>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Product;
