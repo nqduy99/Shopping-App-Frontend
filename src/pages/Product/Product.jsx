962f5fbd0126ef4472dd2725ce379971292b5df0
nqduy99 <nqduy98999@gmail.com>
2022-12-24 14:15:57 +0700

    react e-commerce fetch a single product

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -3,68 +3,68 @@ import './Product.scss';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import BalanceIcon from '@mui/icons-material/Balance';
+import useFetch from '../../hooks/useFetch';
+import { useParams } from 'react-router-dom';
 
 const Product = () => {
-    const [selectedImg, setSelectedImg] = useState(0);
+    const id = useParams().id;
+    const [selectedImg, setSelectedImg] = useState("img");
     const [quantity, setQuantity] = useState(1);
 
-    const images = [
-        'https://vtv1.mediacdn.vn/2022/10/21/gettyimages-1656681307-1666320628461839905497.jpg',
-        'https://image.thanhnien.vn/w1024/Uploaded/2022/zxaijr/2021_12_30/moneycualisalapkyluc3-8661.png',
-    ];
-
+    const {data, loading, error} = useFetch(`/products/${id}?populate=*`);
+    
     return (
         <div className="product">
-            <div className="left">
-                <div className="images">
-                    <img src={images[0]} alt="productImg01" onClick={(e) => setSelectedImg(0)} />
-                    <img src={images[1]} alt="productImg02" onClick={(e) => setSelectedImg(1)} />
-                </div>
-                <div className="main">
-                    <div className="mainImg">
-                        <img src={images[selectedImg]} alt="" />
-                    </div>
+            {loading ? "" 
+            : 
+            (<><div className="left">
+            <div className="images">
+                <img src={process.env.REACT_APP_UPLOAD_URL + data?.attributes?.img?.data?.attributes?.url} alt="productImg01" onClick={(e) => setSelectedImg("img")} />
+                <img src={process.env.REACT_APP_UPLOAD_URL + data?.attributes?.img2?.data?.attributes?.url} alt="productImg02" onClick={(e) => setSelectedImg("img2")} />
+            </div>
+            <div className="main">
+                <div className="mainImg">
+                    <img src={process.env.REACT_APP_UPLOAD_URL + data?.attributes[selectedImg].data?.attributes?.url} alt="" />
                 </div>
             </div>
-            <div className="right">
-                <h1>Title</h1>
-                <span className='price'>$199</span>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                    et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas
-                    accumsan lacus vel facilisis labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.
-                    Risus commodo viverra maecenas.
-                </p>
-                <div className="quantity">
-                    <button onClick={() => setQuantity((prev) => (prev === 1 ? 1 : prev - 1))}>-</button>
-                    {quantity}
-                    <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
+        </div>
+        <div className="right">
+            <h1>{data?.attributes?.title}</h1>
+            <span className='price'>${data?.attributes?.price}</span>
+            <p>
+                {data?.attributes?.desc}
+            </p>
+            <div className="quantity">
+                <button onClick={() => setQuantity((prev) => (prev === 1 ? 1 : prev - 1))}>-</button>
+                {quantity}
+                <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
+            </div>
+            <button className="add">
+                <AddShoppingCartIcon /> ADD TO CART
+            </button>
+            <div className="links">
+                <div className="item">
+                    <FavoriteBorderIcon /> ADD TO WISHLIST
                 </div>
-                <button className="add">
-                    <AddShoppingCartIcon /> ADD TO CART
-                </button>
-                <div className="links">
-                    <div className="item">
-                        <FavoriteBorderIcon /> ADD TO WISHLIST
-                    </div>
-                    <div className="item">
-                        <BalanceIcon /> ADD TO COMPARE
-                    </div>
+                <div className="item">
+                    <BalanceIcon /> ADD TO COMPARE
                 </div>
-                <div className="info">
-                    <span>Vendor: Polo</span>
-                    <span>Product Type: T-Shirt</span>
-                    <span>Tag: T-Shirt, Women, Top</span>
-                </div>
+            </div>
+            <div className="info">
+                <span>Vendor: Polo</span>
+                <span>Product Type: T-Shirt</span>
+                <span>Tag: T-Shirt, Women, Top</span>
+            </div>
+            <hr />
+            <div className="info">
+                <span>DESCRIPTION</span>
+                <div>{data?.attributes?.desc}</div>
                 <hr />
-                <div className="info">
-                    <span>DESCRIPTION</span>
-                    <hr />
-                    <span>ADDITIONAL INFORMATION</span>
-                    <hr />
-                    <span>FAQ</span>
-                </div>
+                <span>ADDITIONAL INFORMATION</span>
+                <hr />
+                <span>FAQ</span>
             </div>
+        </div></>)}
         </div>
     );
 };
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -7,7 +7,7 @@ import useFetch from './../../hooks/useFetch';
 const Products = () => {
     const catId = parseInt(useParams().id);
     const [maxPrice, setMaxPrice] = useState(1000);
-    const [sort, setSort] = useState(null);
+    const [sort, setSort] = useState("asc");
     const [selectedSubCats, setSelectedSubCats] = useState([])
     // console.log(catId);
 
