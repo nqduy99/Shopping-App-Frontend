bcff68f1e28517bd2138ad1941a4276dd2a8e7ad
nqduy99 <nqduy98999@gmail.com>
2022-12-24 15:37:26 +0700

    react redux shopping cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,51 +1,42 @@
+import { useDispatch, useSelector } from 'react-redux';
 import { DeleteOutline } from '@mui/icons-material';
 import React from 'react';
 import './Cart.scss';
+import { removeItem, resetCart } from '../../redux/cartReducer';
 
 const Cart = () => {
-    const data = [
-        {
-            id: 1,
-            img: 'https://i.bloganchoi.com/bloganchoi.com/wp-content/uploads/2022/07/lisa-4-blackpink-696x1046.jpg?fit=700%2C20000&quality=95&ssl=1',
-            img2: 'https://vcdn1-giaitri.vnecdn.net/2021/11/24/lisa-2554-1637749826.jpg?w=680&h=0&q=100&dpr=1&fit=crop&s=ybWaQjRUALuxjSGVNvqnVQ',
-            title: 'Lisa',
-            desc: 'Lisa yeu Duy',
-            isNew: true,
-            oldPrice: 19,
-            price: 12,
-        },
-        {
-            id: 2,
-            img: 'https://i.bloganchoi.com/bloganchoi.com/wp-content/uploads/2022/07/lisa-4-blackpink-696x1046.jpg?fit=700%2C20000&quality=95&ssl=1',
-            img2: 'https://vcdn1-giaitri.vnecdn.net/2021/11/24/lisa-2554-1637749826.jpg?w=680&h=0&q=100&dpr=1&fit=crop&s=ybWaQjRUALuxjSGVNvqnVQ',
-            title: 'Lisa',
-            desc: 'Lisa yeu Duy',
-            isNew: true,
-            oldPrice: 19,
-            price: 12,
-        },
-    ];
+    const products = useSelector(state=>state.cart.products)
+    const dispatch = useDispatch()
+
+
+    const totalPrice = () => {
+        let total = 0
+        products.forEach((item)=> {
+            total+=item.quantity*item.price
+        });
+        return total.toFixed(2);
+    }
 
     return (
         <div className="cart">
             <h1>Products in you cart</h1>
-            {data.map((item) => (
+            {products.map((item) => (
                 <div className="item" key={item.id}>
-                    <img src={item.img} alt="" />
+                    <img src={process.env.REACT_APP_UPLOAD_URL + item.img} alt="" />
                     <div className="details">
                         <h1>{item.title}</h1>
                         <p>{item.desc?.substring(0, 100)}</p>
-                        <div className="price">1 x ${item.price}</div>
+                        <div className="price">{item.quantity} x ${item.price}</div>
                     </div>
-                    <DeleteOutline className="delete" />
+                    <DeleteOutline className="delete" onClick={()=>dispatch(removeItem(item.id))} />
                 </div>
             ))}
             <div className="total">
                 <span className="">SUBTOTAL</span>
-                <span>$123</span>
+                <span>${totalPrice()}</span>
             </div>
             <button>PROCEED TO CHECKOUT</button>
-            <span className="reset">Reset Cart</span>
+            <span className="reset" onClick={()=>dispatch(resetCart())}>Reset Cart</span>
         </div>
     );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,9 +8,11 @@ import { Link } from 'react-router-dom';
 
 import './Navbar.scss';
 import Cart from '../Cart/Cart';
+import { useSelector } from 'react-redux';
 
 const Navbar = () => {
     const [open, setOpen] = useState(false);
+    const products = useSelector(state=>state.cart.products)
 
     return (
         <div className="navbar">
@@ -72,7 +74,7 @@ const Navbar = () => {
                         <FavoriteBorderOutlinedIcon />
                         <div className="cartIcon" onClick={() => setOpen(!open)}> 
                             <ShoppingCartOutlinedIcon />
-                            <span>0</span>
+                            <span>{products.length}</span>
                         </div>
                     </div>
                 </div>
