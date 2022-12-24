7ca69da0d79623e529e108d4f3bbc9a2513e87c3
nqduy99 <nqduy98999@gmail.com>
2022-12-24 13:35:03 +0700

    react strapi rest api filtering tutorial

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,70 +1,19 @@
 import React from 'react';
+
 import './List.scss';
 import Card from '../Card/Card';
+import useFetch from '../../hooks/useFetch';
 
-const List = () => {
-    const data = [
-        {
-            id: 1,
-            img: 'https://i.bloganchoi.com/bloganchoi.com/wp-content/uploads/2022/07/lisa-4-blackpink-696x1046.jpg?fit=700%2C20000&quality=95&ssl=1',
-            img2: 'https://vcdn1-giaitri.vnecdn.net/2021/11/24/lisa-2554-1637749826.jpg?w=680&h=0&q=100&dpr=1&fit=crop&s=ybWaQjRUALuxjSGVNvqnVQ',
-            title: 'Lisa',
-            isNew: true,
-            oldPrice: 19,
-            price: 12,
-        },
-        {
-            id: 2,
-            img: 'https://i.bloganchoi.com/bloganchoi.com/wp-content/uploads/2022/07/lisa-4-blackpink-696x1046.jpg?fit=700%2C20000&quality=95&ssl=1',
-            img2: 'https://vcdn1-giaitri.vnecdn.net/2021/11/24/lisa-2554-1637749826.jpg?w=680&h=0&q=100&dpr=1&fit=crop&s=ybWaQjRUALuxjSGVNvqnVQ',
-            title: 'Lisa',
-            isNew: true,
-            oldPrice: 19,
-            price: 12,
-        },
-        {
-            id: 3,
-            img: 'https://i.bloganchoi.com/bloganchoi.com/wp-content/uploads/2022/07/lisa-4-blackpink-696x1046.jpg?fit=700%2C20000&quality=95&ssl=1',
-            img2: 'https://vcdn1-giaitri.vnecdn.net/2021/11/24/lisa-2554-1637749826.jpg?w=680&h=0&q=100&dpr=1&fit=crop&s=ybWaQjRUALuxjSGVNvqnVQ',
-            title: 'Lisa',
-            isNew: true,
-            oldPrice: 19,
-            price: 12,
-        },
-        {
-            id: 4,
-            img: 'https://i.bloganchoi.com/bloganchoi.com/wp-content/uploads/2022/07/lisa-4-blackpink-696x1046.jpg?fit=700%2C20000&quality=95&ssl=1',
-            img2: 'https://vcdn1-giaitri.vnecdn.net/2021/11/24/lisa-2554-1637749826.jpg?w=680&h=0&q=100&dpr=1&fit=crop&s=ybWaQjRUALuxjSGVNvqnVQ',
-            title: 'Lisa',
-            isNew: true,
-            oldPrice: 19,
-            price: 12,
-        },
-        {
-            id: 5,
-            img: 'https://i.bloganchoi.com/bloganchoi.com/wp-content/uploads/2022/07/lisa-4-blackpink-696x1046.jpg?fit=700%2C20000&quality=95&ssl=1',
-            img2: 'https://vcdn1-giaitri.vnecdn.net/2021/11/24/lisa-2554-1637749826.jpg?w=680&h=0&q=100&dpr=1&fit=crop&s=ybWaQjRUALuxjSGVNvqnVQ',
-            title: 'Lisa',
-            isNew: true,
-            oldPrice: 19,
-            price: 12,
-        },
-        {
-            id: 6,
-            img: 'https://i.bloganchoi.com/bloganchoi.com/wp-content/uploads/2022/07/lisa-4-blackpink-696x1046.jpg?fit=700%2C20000&quality=95&ssl=1',
-            img2: 'https://vcdn1-giaitri.vnecdn.net/2021/11/24/lisa-2554-1637749826.jpg?w=680&h=0&q=100&dpr=1&fit=crop&s=ybWaQjRUALuxjSGVNvqnVQ',
-            title: 'Lisa',
-            isNew: true,
-            oldPrice: 19,
-            price: 12,
-        },
-    ];
+const List = ({catId, subCats, maxPrice, sort}) => {
 
-    return <div className='list'>{
-        data?.map(item => (
+    const {data, loading, error} = useFetch(`/products?populate=*&[filters][categories][id]=${catId}${subCats.map((item)=> `&[filters][sub_categories][id][$eq]=${item}`)}&[filters][price][$lte]=${maxPrice}&sort=price:${sort}`);
+    // console.log(catId);
+
+    return <div className='list'>
+        {loading ? "loading" : data?.map(item => (
             <Card item={item} key={item.id} />
-        ))
-    }</div>;
+        ))}
+    </div>;
 };
 
 export default List;
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -2,29 +2,35 @@ import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import List from '../../components/List/List';
 import './Products.scss';
+import useFetch from './../../hooks/useFetch';
 
 const Products = () => {
     const catId = parseInt(useParams().id);
     const [maxPrice, setMaxPrice] = useState(1000);
-    const [sort, setSort] = useState(null)
+    const [sort, setSort] = useState(null);
+    const [selectedSubCats, setSelectedSubCats] = useState([])
+    // console.log(catId);
+
+    const {data, loading, error} = useFetch(`/sub-categories?[filters][categories][id][$eq]=${catId}`)
+
+    const handleChange = (e) => {
+        const value = e.target.value;
+        const isChecked = e.target.checked;
+        
+        setSelectedSubCats(isChecked ? [...selectedSubCats, value] : selectedSubCats.filter(item => item !== value))
+    }
 
     return (
         <div className="products">
             <div className="left">
                 <div className="filterItem">
                     <h2>Product Categories</h2>
-                    <div className="inputItem">
-                        <input type="checkbox" id="1" value={1} />
-                        <label htmlFor="1">Shoes</label>
-                    </div>
-                    <div className="inputItem">
-                        <input type="checkbox" id="2" value={2} />
-                        <label htmlFor="2">Skirts</label>
-                    </div>
-                    <div className="inputItem">
-                        <input type="checkbox" id="3" value={3} />
-                        <label htmlFor="3">Coats</label>
-                    </div>
+                    {data?.map((item)=>(
+                        <div className="inputItem" key={item.id}>
+                            <input type="checkbox" id={item.id} value={item.id} onChange={handleChange}/>
+                            <label htmlFor={item.id}>{item.attributes.title}</label>
+                        </div>
+                    ))}
                 </div>
                 <div className="filterItem">
                     <h2>Filter by price</h2>
@@ -52,7 +58,7 @@ const Products = () => {
                     src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600"
                     alt=""
                 />
-                <List catId={catId} maxPrice={maxPrice} sort={sort} />
+                <List catId={catId} maxPrice={maxPrice} sort={sort} subCats={selectedSubCats}/>
             </div>
         </div>
     );
